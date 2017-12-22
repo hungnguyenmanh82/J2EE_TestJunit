25c7e8a1bbe745112c4fd17908a69afcdb5c0c8b
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2017-12-22 16:56:04 +0700

    show log on the console

--- src/test/java/test/TestJunit1.java
+++ src/test/java/test/TestJunit1.java
@@ -6,22 +6,26 @@ import org.junit.Test;
 public class TestJunit1 {
     @Test
     public void testcase1() {
+    	System.out.println("====================== testcase1 is running");
         Assert.assertEquals(1, 1);
     }
     
     @Test
     public void testcase2() {
+    	System.out.println("====================== testcase2 is running");
     	int result = 1 + 5;
         Assert.assertTrue(result == 6);;
     }
     
     @Test
     public void testcase3() {
+    	System.out.println("====================== testcase3 is running");
         Assert.assertEquals(1, 1);
     }
     
     @Test
     public void testcase4() {
+    	System.out.println("====================== testcase4 is running");
         Assert.assertEquals(1, 1);
     }
 }
